81ff44e57c3e7559b7f8453b7253c1246f97034d
rafi_ahmed <rafiiitju282@gmail.com>
2024-12-24 23:45:33 +0600

    added get route for my tutorials

--- index.js
+++ index.js
@@ -1,15 +1,12 @@
-const express = require('express')
-const app = express()
-require('dotenv').config()
-const cors = require('cors')
+const express = require("express");
+const app = express();
+require("dotenv").config();
+const cors = require("cors");
 const port = process.env.PORT || 5000;
-const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 
-
-app.use(cors())
-app.use(express.json())
-
-
+app.use(cors());
+app.use(express.json());
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.bbiovs6.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
@@ -19,52 +16,79 @@ const client = new MongoClient(uri, {
     version: ServerApiVersion.v1,
     strict: true,
     deprecationErrors: true,
-  }
+  },
 });
 
 async function run() {
   try {
-    
     const tutorCollection = client.db("tutorBooking").collection("tutors");
     const bookCollection = client.db("tutorBooking").collection("bookRecord");
-    
 
-    app.get('/findTutor', async (req, res)=>{
-        const query = {};
-        const result = await tutorCollection.find(query).toArray()
-        res.send(result)
-    })
-    app.get('/tutor/:id', async (req, res)=>{
-        const id = req.params.id;
-        console.log(id);
-        const query = {_id: new ObjectId(id)};
+    app.get("/findTutor", async (req, res) => {
+      const query = {};
+      const result = await tutorCollection.find(query).toArray();
+      res.send(result);
+    });
+    app.get("/tutor/:id", async (req, res) => {
+      const id = req.params.id;
+      console.log(id);
+      const query = { _id: new ObjectId(id) };
 
-        const result = await tutorCollection.findOne(query)
-        res.send(result)
-    })
+      const result = await tutorCollection.findOne(query);
+      res.send(result);
+    });
 
-    app.post('/addTutorials', async (req, res)=>{
-        const tutorialsData = req.body;
-        const result = await tutorCollection.insertOne(tutorialsData);
+    app.post("/addTutorials", async (req, res) => {
+      const tutorialsData = req.body;
+      const result = await tutorCollection.insertOne(tutorialsData);
 
-        res.send(result);
-    })
-    app.post('/bookTutorials', async (req, res)=>{
-        const bookData = req.body;
-        console.log(bookData);
-        const result = await bookCollection.insertOne(bookData);
+      res.send(result);
+    });
+    app.post("/bookTutorials", async (req, res) => {
+      const bookData = req.body;
+      console.log(bookData);
+      const result = await bookCollection.insertOne(bookData);
 
-        res.send(result);
-    })
+      res.send(result);
+    });
 
-    app.get('/bookedTutorials', async (req, res)=>{
-        const query = {}
-        const result = await bookCollection.find(query).toArray()
-        res.send(result)
-    })
-    
-    
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    app.get("/bookedTutorials", async (req, res) => {
+      const query = {};
+      const result = await bookCollection.find(query).toArray();
+      res.send(result);
+    });
+
+    // app.patch("/tutor/:id", async (req, res) => {
+    //   const id = req.params.id;
+    //   const { review } = req.body;
+    //   // console.log(id);
+    //   const query = { _id: new ObjectId(id) };
+    //   const options = { upsert: true };
+    //   const updateDoc = {
+    //     $inc: {
+    //       review: 1,
+    //     },
+    //   };
+    //   const result = await tutorCollection.updateOne(query, updateDoc, options);
+    //   console.log(result);
+
+    //   res.send(result);
+    // });
+
+    app.get("/tutorr/:ema", async (req, res) => {
+      const email = req.params.ema;
+      
+      const query = {
+        email: email,
+      };
+
+      const result = await tutorCollection.find(query).toArray();
+      res.send(result);
+    });
+
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
   } finally {
     // Ensures that the client will close when you finish/error
     // await client.close();
@@ -72,11 +96,10 @@ async function run() {
 }
 run().catch(console.dir);
 
-
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+  console.log(`Example app listening on port ${port}`);
+});
